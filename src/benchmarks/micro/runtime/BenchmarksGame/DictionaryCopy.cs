e7fe378d00d5c232593a5175ce43f3ce6c94b24d
John Call <johncall@microsoft.com>
2020-12-01 09:03:05 -0800

    Add DictionaryCopy

---
+++ src/benchmarks/micro/runtime/BenchmarksGame/DictionaryCopy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using MicroBenchmarks;
+
+namespace MicroBenchmarks.runtime
+{
+    [BenchmarkCategory(Categories.Libraries, Categories.Collections, Categories.GenericCollections)]
+    public class DictionaryCopy
+    {
+        private static Dictionary<string, int> FullDictionary;
+        
+        private static Dictionary<string, int> HalfEmptyDictionary;
+
+        static DictionaryCopy()
+        {
+            var size = 3000;
+            FullDictionary = new Dictionary<string, int>(size, StringComparer.OrdinalIgnoreCase);
+            HalfEmptyDictionary = new Dictionary<string, int>(size, StringComparer.OrdinalIgnoreCase);
+            for (int i = 0; i < size; i++)
+            {
+                HalfEmptyDictionary[$"Test{i}"] = i;
+                FullDictionary[$"Test{i}"] = i;
+            }
+            for (int i = 0; i < size; i += 2)
+            {
+                HalfEmptyDictionary.Remove($"Test{i}");
+            }
+        }
+
+        [Benchmark]
+        public void CopyConstructorFull()
+        {
+            new Dictionary<string, int>(FullDictionary, StringComparer.OrdinalIgnoreCase);
+        }
+
+        [Benchmark]
+        public void CopyConstructorHalfEmpty()
+        {
+            new Dictionary<string, int>(HalfEmptyDictionary, StringComparer.OrdinalIgnoreCase);
+        }
+    }
+}
